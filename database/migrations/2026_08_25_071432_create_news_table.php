9ab9abbc10b3d1b57c8b0d88e1e188a428ed929e
chayma <chayma.abdessadak@student.ehb.be>
2026-08-25 09:22:33 +0200

    News tabel kolommen toegevoegd: title, image, content, published date

--- database/migrations/2026_08_25_071432_create_news_table.php
+++ database/migrations/2026_08_25_071432_create_news_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('image')->nullable();
+            $table->text('content');
+            $table->date('published_date');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('news');
