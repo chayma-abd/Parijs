03f683db79ac5820bfe16e34d6b3dbe505dd2e7c
chayma <chayma.abdessadak@student.ehb.be>
2026-08-26 12:34:10 +0200

    Route voor nieuws verwijderen toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,6 @@ Route::get('/news/create', [NewsController::class, 'create'])->name('news.create
 Route::post('/news', [NewsController::class, 'store'])->name('news.store');
 Route::get('/news/{id}/edit', [NewsController::class, 'edit'])->name('news.edit');
 Route::put('/news/{id}', [NewsController::class, 'update'])->name('news.update');
+Route::delete('/news/{id}', [NewsController::class, 'destroy'])->name('news.destroy');
 
 require __DIR__.'/auth.php';
