cec049ff9f1498887c78cc83cfab63b7587e2bf0
chayma <chayma.abdessadak@student.ehb.be>
2026-08-26 18:32:26 +0200

    ContactMail vereenvoudigd

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -2,53 +2,25 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
+ use Illuminate\Bus\Queueable;
+ use Illuminate\Mail\Mailable;
+ use Illuminate\Queue\SerializesModels;
 
 class ContactMail extends Mailable
-{
+ {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public $data;
+
+    public function __construct($data)
     {
-        //
+        $this->data = $data;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+ public function build()
     {
-        return new Envelope(
-            subject: 'Contact Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
-}
+        return $this->from($this->data['email'])
+                    ->subject('Nieuw contactbericht van ' . $this->data['name'])
+                    ->view('emails.contact');
+  }
+ }
