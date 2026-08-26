33a6f6851bb72c4e88b7b5abef7107171a9771e7
chayma <chayma.abdessadak@student.ehb.be>
2026-08-26 18:34:41 +0200

    ContactController: email functionaliteit toegevoegd

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\ContactMail;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -18,6 +20,14 @@ class ContactController extends Controller
             'email' => 'required|email',
             'message' => 'required',
         ]);
+
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'message' => $request->message,
+        ];
+
+        Mail::to('admin@ehb.be')->send(new ContactMail($data));
 
         return redirect()->route('contact.show')->with('success', 'Je bericht is verstuurd!');
     }
