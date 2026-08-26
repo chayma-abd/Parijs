487f3d60ecb65cba8895efb0422b78d2d2ea33db
chayma <chayma.abdessadak@student.ehb.be>
2026-08-26 17:43:04 +0200

    FaqController index methode toegevoegd

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Faq;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::with('faqs')->get();
+        return view('faq.index', compact('categories'));
+    }
 }
