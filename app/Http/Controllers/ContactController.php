3c9c99a9a0a5b471c8355f2d4fd062509a614e68
chayma <chayma.abdessadak@student.ehb.be>
2026-08-26 18:24:52 +0200

    ContactController: show en send methodes toegevoegd

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('contact.show');
+    }
+
+    public function send(Request $request)
+    {
+        return redirect()->route('contact.show')->with('success', 'Je bericht is verstuurd!');
+    }
 }
